e8c1ff8da4f3ee1d2a3c1c02b8d88376734e440e
Shreyas <mandlikshreyas01@gmail.com>
2022-09-04 22:21:04 +0530

    Initial Testcase Completed

--- FizBuz.py
+++ FizBuz.py
@@ -1,10 +1,5 @@
 def fizzBuzz(n):
     list1=[]
     for i in range(1,n+1):
-        if i%3 == 0 and i>=3:
-            list1.append("Fizz")
-        elif i%5 == 0 and i>=5:
-            list1.append("Buzz")
-        else:
             list1.append(i)
     return list1
--- test_Fizzbuzz.py
+++ test_Fizzbuzz.py
@@ -1,5 +1,13 @@
 import unittest
+from unittest import result
 import FizBuz
 class Test(unittest.TestCase):
-    pass
+    
+
+    def test_Print_the_number_itself(self):
+        result=FizBuz.fizzBuzz(1)
+        self.assertEqual(result,[1])
+
+        result1=FizBuz.fizzBuzz(2)
+        self.assertEqual(result1,[1,2])
     
