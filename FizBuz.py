b5152ccf9a1bc64a72b34404f833f3a1ef92a5d1
Shreyas <mandlikshreyas01@gmail.com>
2022-09-04 22:30:10 +0530

    Buzz returned for multiple 5

--- FizBuz.py
+++ FizBuz.py
@@ -3,6 +3,10 @@ def fizzBuzz(n):
     for i in range(1,n+1):
         if i%3 == 0 and i>=3:
             list1.append("Fizz")
+        elif i%5 == 0 and i>=5:
+            list1.append("Buzz")
         else:
             list1.append(i)
     return list1
+
+print(fizzBuzz(10))
--- test_Fizzbuzz.py
+++ test_Fizzbuzz.py
@@ -15,3 +15,17 @@ class Test(unittest.TestCase):
     def test_Print_Multiple_of_3(self):
         result=FizBuz.fizzBuzz(3)
         self.assertEqual(result,[1,2,"Fizz"])
+
+        result1=FizBuz.fizzBuzz(9)
+        self.assertEqual(result1,[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz'])
+
+
+    def test_Print_Multiple_of_5(self):
+        result=FizBuz.fizzBuzz(5)
+        self.assertEqual(result,[1, 2, 'Fizz', 4, 'Buzz'])
+
+        result1=FizBuz.fizzBuzz(10)
+        self.assertEqual(result1,[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz'])
+
+
+
